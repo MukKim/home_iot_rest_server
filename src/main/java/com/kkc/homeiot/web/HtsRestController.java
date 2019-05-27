bd1cf3228738e91ad77cbc0b1e3864933f8d4c14
khe0613 <khe0616@cnu.ac.kr>
2019-05-27 11:14:14 +0900

    Controller 파일 생성(구현 x)

---
+++ src/main/java/com/kkc/homeiot/web/HtsRestController.java
@@ -0,0 +1,9 @@
+package com.kkc.homeiot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hts")
+public class HtsRestController {
+}
---
+++ src/main/java/com/kkc/homeiot/web/MemberRestController.java
@@ -0,0 +1,14 @@
+package com.kkc.homeiot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("member")
+public class MemberRestController {
+
+
+
+
+
+}
